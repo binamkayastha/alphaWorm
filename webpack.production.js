f54b45d28bdcbbc8009a526fdaf620fe3fef0fcc
christikaes <christikaes@gmail.com>
2016-06-25 18:09:03 -0400

    FINAL INITIAL STUB

--- webpack.production.js
+++ webpack.production.js
@@ -7,21 +7,15 @@ var devFlagPlugin = new webpack.DefinePlugin({
 });
 
 module.exports = {
-  devtool: 'cheap-module-eval-source-map',
   entry: [
-    'eventsource-polyfill',
-    'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000',
     './js/index.js'
   ],
   output: {
     path: path.join(__dirname, 'dist'),
     publicPath: '/static/',
     filename: 'bundle.js',
-    hot: true
   },
   plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin(),
     devFlagPlugin,
     new ExtractTextPlugin('app.css')
